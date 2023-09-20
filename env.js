a677cd9c0da8511cd40a21e10f7adbf4d97c66f9
Curtis <trucdev@gmail.com>
2023-09-20 18:47:12 +0700

    WIP

--- env.js
+++ env.js
@@ -1,7 +1,41 @@
 require("dotenv").config();
+const {machineIdSync} = require('node-machine-id');
+const crypto = require('crypto');
+
+const machineId = machineIdSync();
+const buffMachineId = Buffer.from(machineId, 'hex');
+const CODE = buffMachineId.toString('base64url');
+
+function generatePass(){
+  const secret_key = '+HW5jB8"]!xD';
+  const key = crypto
+  .createHash('sha512')
+  .update(secret_key)
+  .digest('hex')
+  .substring(0, 32)
+
+  const secret_iv = 'okeh{Thf#Azl';
+  const encryptionIV = crypto
+  .createHash('sha512')
+  .update(secret_iv)
+  .digest('hex')
+  .substring(0, 16)
+  
+  const cipher = crypto.createCipheriv('aes-256-cbc', key, encryptionIV)
+
+  const pass = Buffer.from(
+    cipher.update(CODE, 'utf8', 'hex') + cipher.final('hex')
+  ).toString('base64') 
+
+  return {
+    pass,
+    ver: '0.1'
+  }
+}
+
+const SECRET = generatePass();
 
 const ENDPOINT = process.env.ENDPOINT;
-const CODE = process.env.CODE;
 const SCRETKEY = process.env.SCRETKEY;
 const REQUEST_TIMEOUT = +process.env.REQUEST_TIMEOUT;
 const TIMEOUT_TO_DELAY_WHEN_INACTIVE = +process.env.TIMEOUT_TO_DELAY_WHEN_INACTIVE;
