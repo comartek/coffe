3eccea82c367ae379fd038006af7c74e575a8808
Curtis <trucdev@gmail.com>
2023-11-24 13:15:15 +0700

    WIP

--- device.js
+++ device.js
@@ -2,9 +2,12 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 
+const debounce = require("lodash.debounce");
 const rpio = require("rpio");
 
 const PIN = 12;
+const COUNTER_PIN = 22;
+
 const PULSE_WIDTH = 25; // ms
 const TIMEOUT_TO_END_COMMAND = PULSE_WIDTH * 10;
 
@@ -19,30 +22,61 @@ const sendStatusSignal = (status) => {
   // todo:
 };
 
+const msleep = (ms) => {
+  return new Promise((rs) => {
+    setTimeout(rs, ms);
+  });
+};
+
 const sendMoneySignal = async (val) => {
   console.log("val=>>>>>>>>>>>>>>>>>>>", val);
   if (val == 0) {
     rpio.close(PIN);
     return;
   }
+
   rpio.open(PIN, rpio.OUTPUT, rpio.LOW);
-  rpio.msleep(TIMEOUT_TO_END_COMMAND);
+  await msleep(TIMEOUT_TO_END_COMMAND);
 
   for (let i = 0; i < val; i++) {
     rpio.open(PIN, rpio.OUTPUT, rpio.LOW);
-    rpio.msleep(PULSE_WIDTH * 4);
+    await msleep(PULSE_WIDTH * 4);
 
     rpio.open(PIN, rpio.OUTPUT, rpio.HIGH);
-    rpio.msleep(PULSE_WIDTH);
+    await msleep(PULSE_WIDTH);
   }
   rpio.open(PIN, rpio.OUTPUT, rpio.LOW);
-  rpio.msleep(TIMEOUT_TO_END_COMMAND);
-  
+  await msleep(TIMEOUT_TO_END_COMMAND);
   rpio.close(PIN);
 };
 
+let callbackOnPay = (value) => {
+  console.log("report value");
+};
+let counterPollLow = 0;
+
+const setCallbackOnPay = (callback) => {
+  callbackOnPay = callback;
+};
+
+const debounceReceiptPayment = debounce(() => {
+  const total = counterPollLow;
+  counterPollLow = 0;
+  if (callbackOnPay && total > 0) {
+    callbackOnPay(total);
+  }
+}, 1000);
+
+const onReceiptPaymentSignal = (pin) => {
+  counterPollLow++;
+  debounceReceiptPayment();
+};
+
+rpio.open(COUNTER_PIN, rpio.INPUT);
+rpio.poll(COUNTER_PIN, onReceiptPaymentSignal, rpio.POLL_LOW);
 
 module.exports = {
   sendMoneySignal,
   sendStatusSignal,
+  setCallbackOnPay,
 };
--- env.js
+++ env.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const fs = require('fs')
 const { getDeviceCode } = require("./make-device-code");
 
-const VERSION = "0.1.10";
+const pjson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+
+const VERSION = pjson.version;
 const SECRET = "omupfsvmek";
 
 const CODE = getDeviceCode();
@@ -17,7 +20,8 @@ const FUNC_KEY = () => jwt.sign(
   }
 );
 
-const ENDPOINT = "https://api-cafe.viziple.com";
+const ENDPOINT = "https://api-cafe-dev.viziple.com";
+// const ENDPOINT = "http://localhost:3456";
 const REQUEST_TIMEOUT = 3000;
 const TIMEOUT_TO_DELAY_WHEN_ERROR = 500;
 const TIMEOUT_TO_DELAY_WHEN_INACTIVE = 250;
